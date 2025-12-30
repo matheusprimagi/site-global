daf946d3aa9bc4d41e01479be9d6faf1e5b925c3
matheusprimagi <matheusprimagi@gmail.com>
2025-12-30 15:30:17 -0300

    segurança basica

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const fs = require("fs");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "1kb" }));
 app.use(express.static("public"));
 
 app.post("/salvar", (req, res) => {
-  fs.writeFileSync("texto.txt", req.body.texto);
+  const texto = req.body.texto;
+
+  if (typeof texto !== "string" || texto.length > 200) {
+    return res.status(400).send("Texto inválido");
+  }
+
+  fs.writeFileSync("texto.txt", texto);
   res.sendStatus(200);
 });
+
 
 app.get("/texto", (req, res) => {
   const texto = fs.existsSync("texto.txt")
@@ -23,5 +30,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT);
 });
+
 
 
